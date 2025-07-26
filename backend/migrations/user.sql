2f4a3cb2926a3a80aa068959879addef6ea27a4d
unknown <abhuradia15@gmail.com>
2025-07-27 01:41:21 +0530

    first commit

--- backend/migrations/user.sql
+++ backend/migrations/user.sql
@@ -2,5 +2,5 @@ CREATE TABLE user_details (
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL
+    password TEXT NOT NULL
 );
